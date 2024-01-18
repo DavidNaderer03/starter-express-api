85f46ebb2b2997f52ffada8d1c5ffe4619758883
DavidNaderer03 <aocnaderer@gmail.com>
2024-01-18 21:41:45 +0100

    asdf

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 
 app.use(express.static('./public'))
 
+app.get('/api/random', (req, res) => {
+    return res.json("Hello");
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Port listen to localhost:${process.env.PORT || 3000}`);
 });
