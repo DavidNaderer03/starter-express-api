5a32cf9428649eab39a3ff45d799f85af883c629
DavidNaderer01 <dav1.nad007@gmail.com>
2024-01-18 20:53:01 +0100

    i

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.use(express.static('./public'))
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Port listen to localhost:${process.env.PORT || 3000}`);
+});
