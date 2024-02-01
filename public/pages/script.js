3c8de3bf08a492d428b3e58035429fc19a7ad013
DavidNaderer03 <aocnaderer@gmail.com>
2024-02-01 18:24:39 +0100

    s

--- public/pages/script.js
+++ public/pages/script.js
@@ -1,12 +1,7 @@
-async function log() {
-    let username = document.getElementById('typeUsernameX-2').value;
-    let password = document.getElementById('typePasswordX-2').value;
-    let url = `/api/log?username=${username}&password=${password}`;
-    await fetch(url)
-        .then(data => data.json())
-        .then(json => {
-            if (json.valid) {
-                window.location.href = './logged.html'
-            }
+function Load(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(response => {
+            document.getElementById('content').innerHTML = response;
         });
 }
